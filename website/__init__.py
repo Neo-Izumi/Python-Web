22800c4840cbd66e1321740a51960a088c4584ee
Neo-Izumi <doanhthinhai@gmail.com>
2023-02-09 18:43:44 +0700

    day 3: an error with database occurs. in detail, there is a problem with unique attribute email

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+# this is a fuction that is no longer necessary because from flask-sqlalchemy 3, sqlalchemy will not overwrite the existing database
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print("Database created successfully!")
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET-KEY'] = 'this is a secret key for encrypt my cookies and session data related to my website'
+    app.secret_key = 'super secret key'
+    app.config['SECRET-KEY'] = 'this is a secret key for encrypt my cookies and session data related to my website.'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views 
-    from .auth import auth 
+    from .auth import auth  
     
     app.register_blueprint(views, url_prefix = "/")
     app.register_blueprint(auth, url_prefix = "/")
+    
+    from . import models
+    
+    with app.app_context():
+        db.create_all()
     
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import models, db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import re
 
 auth = Blueprint('ath', __name__)
 
-@auth.route('/login')
+regexEmail = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+regexPassword = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$' 
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<h1> Logout </h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if re.fullmatch(regexEmail, email) == None:
+            flash("Invalid email address !!!", category='error')
+        elif len(firstName) < 2:
+            flash("Your first name must be at least 2 characters", category='error')
+        elif len(lastName) < 2:
+            flash("Your last name must be at least 2 characters", category='error')
+        elif password1 != password2:
+            flash("Confirm password don\'t match", category='error')
+        elif len(password1) < 2:    # re.fullmatch(regexPassword, password1) == None:
+            flash("Your password is too short", category='error')
+        else:
+            new_user = models.User(email=email, first_name=firstName, last_name=lastName, password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account was created successfully", category='success') 
+            return redirect(url_for('.login'))
+        
     return render_template("sign-up.html")
